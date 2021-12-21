2fce1138fccb5ba5d1b0b13754f0b9236cd6ebdd
CubeCypressTest <94074504+CubeCypressTest@users.noreply.github.com>
2021-12-21 15:33:45 +0700

    Update cube.js

--- cube.js
+++ cube.js
@@ -1,9 +1,1 @@
-<<<<<<< HEAD
-<<<<<<< .merge_file_lva9Am
-module.exports={}; // from test 0.44509651304072606
-=======
-module.exports={}; // from test 0.8772200937177412
->>>>>>> .merge_file_O6F59o
-=======
-module.exports={}; // from test 0.8772200937177412
->>>>>>> 301eb6bd8cd11031205ece3d4ff1465efd72d4d9
+module.exports={};
